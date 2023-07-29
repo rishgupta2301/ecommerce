c33a74c108e9c7d91e7127f020fadd8c29c78eaa
Rishabh Gupta <guptarishabh1122@gmail.com>
2023-07-30 02:41:58 +0530

    Implemented Header Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// components
+import Header from "./components/Header/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
